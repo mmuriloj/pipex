f143f94d1a48d451efef08f610fe4c0779f13927
Murilo Monteiro <mmdj5573@gmail.com>
2022-11-30 00:19:52 -0300

    srcs

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,13 +6,13 @@
 /*   By: mumontei <mumontei@42.sp.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 11:01:15 by mumontei          #+#    #+#             */
-/*   Updated: 2022/11/28 17:11:48 by mumontei         ###   ########.fr       */
+/*   Updated: 2022/11/29 14:55:37 by mumontei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-char **get_path(char **envp)
+char **get_path_directory(char **envp)
 {
 	size_t	line;
 	char *path;
@@ -31,3 +31,49 @@ void execute_command(char *cmd, char **envp)
 {
 	env_path(envp);
 }*/
+
+char	*find_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path;
+	char	*temp;
+	int		i;
+
+	paths = get_path_directory(envp);
+	i = 0;
+	while (paths[i])
+	{
+		temp = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(temp, cmd);
+		free(temp);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (0);
+}
+
+void	execute_command(char *argv, char **envp)
+{
+	char	**cmd;
+	int 	i;
+	char	*path;
+	
+	i = -1;
+	cmd = ft_split(argv, ' ');
+	path = find_path(cmd[0], envp);
+	if (!path)	
+	{
+		while (cmd[++i])
+			free(cmd[i]);
+		free(cmd);
+		error_msg("");
+	}
+	if (execve(path, cmd, envp) == -1)
+		error_msg("");
+}
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mumontei <mumontei@42.sp.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 02:10:53 by mumontei          #+#    #+#             */
-/*   Updated: 2022/11/28 17:15:08 by mumontei         ###   ########.fr       */
+/*   Updated: 2022/11/29 15:06:26 by mumontei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,20 @@ void check_args(int argc)
 	}
 }
 
-void child(char *file, int fd[2], char **envp)
+void child(char **argv, int fd[2], char **paths)
 {
 	int	file_in;
 
-	file_in = open(file, O_RDONLY, 0777);
+	file_in = open(argv[1], O_RDONLY, 0777);
 	if (file_in == -1)
-		error_msg(file);
+		error_msg(argv[1]);
 	dup2(file_in, STDIN_FILENO);
 	dup2(fd[1], STDOUT_FILENO);
 	close(file_in);
-	//execute_command()
+	execute_command(argv[2], paths);
 }
 
-void	parent(char **argv, int *fd, char **envp)
+void	parent(char **argv, int *fd, char **paths)
 {
 	int	file_out;
 
@@ -55,22 +55,26 @@ void	parent(char **argv, int *fd, char **envp)
 	dup2(fd[0], STDIN_FILENO);
 	dup2(file_out, STDOUT_FILENO);
 	close(fd[1]);
-	//execute_command()
+	execute_command(argv[3], paths);
 }
 
 int main(int argc, char **argv, char **envp)
 {
 	pid_t	pid;
 	int		pipe_fd[2];
-	char	**path;
 	
 	check_args(argc);
-	path = get_path(envp);
+	
+	//int i = -1;
+	//ft_printf("\n", envp[i]);
+	//while(envp[++i])
+	//	ft_printf(">> %s\n", envp[i]);
+	
 	if (pipe(pipe_fd) == -1)
 		error_msg("");
 	pid = fork();
 	if (pid == 0)
-		child(argv[1], pipe_fd, envp);
+		child(argv, pipe_fd, envp);
 	waitpid(pid, NULL, 0);
 	parent(argv, pipe_fd, envp);
 }
